3204d46e1df7d17b937c8f5c2ab6037f4acd54e5
RaulSaenzJr <raul.saenz2@gmail.com>
2023-07-28 15:48:10 -0500

    Defined Binary Search Tree(BST) and it's help class, Node.

--- EmployeeInfo.cpp
+++ EmployeeInfo.cpp
@@ -2,14 +2,19 @@
 #include <limits>
 
 EmployeeInfo::EmployeeInfo(int id, const std::string &name)
-	: emp_id(id), emp_name(name) {}
-	int EmployeeInfo::getEmployeeName() const
+    : emp_id(id), emp_name(name) {}
+
+int EmployeeInfo::getEmployeeID() const
 {
-	return emp_name;
+    return emp_id;
+}
+
+std::string EmployeeInfo::getEmployeeName() const
+{
+    return emp_name;
 }
 
 void EmployeeInfo::display() const
 {
-	std::cout << "Employee ID: " << emp_id << ", Employee Name: " << emp_name << std::endl;
+    std::cout << "Employee ID: " << emp_id << ", Employee Name: " << emp_name << std::endl;
 }
-
