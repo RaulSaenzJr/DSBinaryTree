260e6f36ed8a1720bc372b36facd5a78cf6e9428
RaulSaenzJr <raul.saenz2@gmail.com>
2023-07-28 16:47:10 -0500

    Made a switch/case for the menu. There is a bug in case 4 that forces the user to press enter to get back to the menu after adding an employee. If there is a solution let me know, please

---
+++ BinarySearchTree.h
@@ -0,0 +1,46 @@
+#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+
+#include "EmployeeInfo.h"
+#include <fstream>
+
+class Node
+{
+public:
+    EmployeeInfo data;
+    Node *left;
+    Node *right;
+
+    Node(const EmployeeInfo &info);
+};
+
+class BinarySearchTree
+{
+private:
+    Node *root;
+
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+
+    void insert(const EmployeeInfo &info);
+    void remove(int emp_id);
+    Node *search(int emp_id) const;
+    void display() const;
+    void saveToFile(const std::string &filename) const;
+    bool searchID(int emp_id) const;
+    Node *searchName(const std::string &name) const;
+
+private:
+    // Recursive helper functions
+    void insertRecursive(Node *node, const EmployeeInfo &info);
+    Node *removeRecursive(Node *node, int emp_id);
+    Node *findMinNode(Node *node) const;
+    void displayRecursive(Node *node, bool isFirstNode, bool isLeftChild) const;
+    void destroyRecursive(Node *node);
+    Node *searchRecursive(Node *node, int emp_id) const;
+    void saveRecursive(Node *node, std::ofstream &file) const;
+    Node *searchNameRecursive(Node *node, const std::string &name) const;
+};
+
+#endif // BINARYSEARCHTREE_H
