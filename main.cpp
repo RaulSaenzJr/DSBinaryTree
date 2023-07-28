9609b728c5914a1f05910f6f1bb3a58c1752c1dc
RaulSaenzJr <raul.saenz2@gmail.com>
2023-07-27 22:32:18 -0500

    added calls to functions that will be made

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include "menuLoop.h"
 
 int main()
 {
-    
+    DisplayMenu displayMenu;
+    BinarySearchTree bst;
+
+    std::string fileName = getFileNameRetry();
+    EmployeeDataReader::readEmployeeData(fileName, bst);
+
+    menuLoop(bst);
+
+    return 0;
 
     return 0;
 }
